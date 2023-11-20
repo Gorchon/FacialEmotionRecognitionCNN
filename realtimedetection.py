6862a5c979cfaa40b65785034d479904bf457c2a
Gorchon <A01254831@tec.mx>
2023-11-20 14:28:35 -0600

    sent all the data to firebase

--- realtimedetection.py
+++ realtimedetection.py
@@ -1,24 +1,27 @@
-# Python file to detect facial emotions in real time and upload them to Firebase
-
 import cv2
 import numpy as np
 import firebase_admin
 from firebase_admin import credentials, db
 import keras 
 from keras.models import model_from_json
+import time
+
 # Initialize Firebase with your service account credentials
 cred = credentials.Certificate('FireBaseKey.json')
 firebase_admin.initialize_app(cred, {
-     'databaseURL':'repalce this with the url of your firebase database'
+     'databaseURL':'https://iot-project-cows-default-rtdb.firebaseio.com/'
 })
 
 # Define a function to upload data to Firebase
 def upload_to_firebase(name, age, emotion):
-    # Create a reference to the specified path in the Realtime Database
-    ref = db.reference('emotions/' + name + '/' + age)
+    # Create a reference to the 'emotions' path in the Realtime Database
+    ref = db.reference('emotions')
 
-    # Set data at the specified path in the Realtime Database
-    ref.set({
+    # Generate a unique key based on time for FIFO order
+    unique_key = str(int(time.time() * 1000))
+
+    # Set data at the 'emotions' path with the unique key
+    ref.child(unique_key).set({
         'name': name,
         'age': age,
         'emotion': emotion
@@ -27,7 +30,7 @@ def upload_to_firebase(name, age, emotion):
 # Define a function to extract features from an image
 def extract_features(image):
     feature = np.array(image)
-    feature = feature.reshape(1,48,48,1)
+    feature = feature.reshape(1, 48, 48, 1)
     return feature/255.0
 
 # Load the trained facial emotion model
@@ -39,35 +42,35 @@ model = model_from_json(model_json)
 model.load_weights("facialemotionmodel.h5")
 
 # Load the Haar cascade for face detection
-haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-face_cascade=cv2.CascadeClassifier(haar_file)
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
 
 # Create a dictionary to map labels to emotion names
-labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
+labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 
 # Start the webcam
-webcam=cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)
 
 while True:
     # Capture a frame from the webcam
-    i,im=webcam.read()
+    i, im = webcam.read()
 
     # Convert the frame to grayscale
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the grayscale frame
-    faces=face_cascade.detectMultiScale(im,1.3,5)
+    faces = face_cascade.detectMultiScale(im, 1.3, 5)
 
     # Process each detected face
-    for (p,q,r,s) in faces:
+    for (p, q, r, s) in faces:
         # Extract the face region from the grayscale frame
-        image = gray[q:q+s,p:p+r]
+        image = gray[q:q+s, p:p+r]
 
         # Draw a rectangle around the face
-        cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
+        cv2.rectangle(im, (p, q), (p+r, q+s), (255, 0, 0), 2)
 
         # Resize the face region to 48x48 pixels
-        image = cv2.resize(image,(48,48))
+        image = cv2.resize(image, (48, 48))
 
         # Extract features from the resized face region
         img = extract_features(image)
@@ -84,10 +87,10 @@ while True:
         upload_to_firebase(name, age, prediction_label)
 
         # Display the predicted emotion on the frame
-        cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
+        cv2.putText(im, '%s' % (prediction_label), (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
 
     # Display the webcam frame
-    cv2.imshow("Output",im)
+    cv2.imshow("Output", im)
 
     # Check for a 'q' key press to quit
     if cv2.waitKey(27) == ord('q'):
